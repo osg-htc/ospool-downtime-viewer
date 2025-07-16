d7531d48b96aaad84caf15e285a48210d2ea5277
Matthew Westphall <westphall@wisc.edu>
2025-07-16 15:23:59 -0500

    Decrease table margins, add light coloring

--- app/downtime-table.tsx
+++ app/downtime-table.tsx
@@ -77,21 +77,21 @@ function pivotDowntimes(downtimes: DowntimesRespose, resourceGroups: ResourceGro
   return Object.values(rgMap)
 }
 
-function DTHeader({children, rowspan = 1, colspan = 1}: {children: any, rowspan?: number, colspan?: number}) {
-  return <th rowSpan={rowspan} colSpan={colspan} className="w-1/2 border border-gray-300 p-4 text-left font-semibold text-gray-900 dark:border-gray-600 dark:text-gray-200">{children}</th>
+function DTHeader({children,  className, rowspan = 1, colspan = 1}: {children: any, className?: string, rowspan?: number, colspan?: number}) {
+  return <th rowSpan={rowspan} colSpan={colspan} className={`w-1/2 border border-gray-300 p-1 text-left text-xs font-semibold text-gray-900 dark:border-gray-600 dark:text-gray-200 ${className}`}>{children}</th>
 }
 
-function DTCell({children}: {children: any}) {
-  return <td className="border border-gray-300 p-4 text-gray-500 dark:border-gray-700 dark:text-gray-400 align-top">{children}</td>
+function DTCell({className, children}: {className?: string, children: any}) {
+  return <td className={`border border-gray-300 p-1 text-xs text-gray-500 dark:border-gray-700 dark:text-gray-400 align-top ${className}`}>{children}</td>
 }
 
-function DTResourceList({downtimes}: {downtimes: ParsedDowntime[]}) {
+function DTResourceList({className, downtimes}: {className?: string, downtimes: ParsedDowntime[]}) {
  const [showMore, setShowMore] = useState(false)
  const maxDtCount = showMore ? 999 : 5
 
  return (
   <React.Fragment>
-    <DTCell>
+    <DTCell className={className}>
       {downtimes.slice(0, maxDtCount).map(dt=>(
         <span className="text-nowrap" key={dt.ResourceName + " " + dt.StartTime}>{dt.ResourceName}<br/></span>
       ))}
@@ -102,12 +102,12 @@ function DTResourceList({downtimes}: {downtimes: ParsedDowntime[]}) {
       {showMore && <span className="cursor-pointer text-indigo-600 dark:text-indigo-400"
         onClick={()=>setShowMore(false)}> show less</span>}
     </DTCell>
-    <DTCell>
+    <DTCell className={className}>
       {downtimes.slice(0, maxDtCount).map(dt=>(
         <span className="text-nowrap" key={dt.ResourceName + " " + dt.StartTime}>{dt.StartDate.toFormat('yyyy-MM-dd')}<br/></span>
       ))}
     </DTCell>
-    <DTCell>
+    <DTCell className={className}>
       {downtimes.slice(0, maxDtCount).map(dt=>(
         <span className="text-nowrap" key={dt.ResourceName + " " + dt.StartTime}>{dt.EndDate.toFormat('yyyy-MM-dd')}<br/></span>
       ))}
@@ -145,7 +145,7 @@ export default function DowntimeTable({ downtimes, resourceGroups }: DowntimeTab
   return (
     <div className="w-full">
       <input 
-        className="shadow appearance-none border rounded max-w-90 w-full py-2 px-3 my-4 border-gray-300 text-gray-700 leading-tight focus:outline-none focus:shadow-outline dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" 
+        className="shadow appearance-none border rounded max-w-90 w-full py-2 px-3 my-4 border-gray-300 text-gray-700 leading-tight text-sm focus:outline-none focus:shadow-outline dark:border-gray-700 dark:text-gray-400 dark:bg-gray-800" 
         type="text" 
         placeholder="Site Name" 
         value={filter}
@@ -153,42 +153,42 @@ export default function DowntimeTable({ downtimes, resourceGroups }: DowntimeTab
       />
 
       <table className="table-auto w-full border-collapse border border-gray-400 bg-white text-sm dark:border-gray-500 dark:bg-gray-800">
-        <thead className="bg-gray-50 dark:bg-gray-700">
+        <thead className="bg-blue-100 dark:bg-gray-700">
           <tr>
-            <DTHeader rowspan={2} colspan={1}>
+            <DTHeader className="bg-blue-100 dark:bg-gray-700" rowspan={2} colspan={1}>
               <div className="flex content-center cursor-pointer" onClick={()=>setSort({SiteName: !sort.SiteName})}><span>Site </span><SortIcon isSorted={sort.SiteName}/></div>
             </DTHeader>
-            <DTHeader colspan={3}>
+            <DTHeader className="bg-teal-100 dark:bg-gray-700" colspan={3}>
               <div className="flex content-center cursor-pointer" onClick={()=>setSort({PastDowntimes: !sort.PastDowntimes})}><span>Past Downtimes </span><SortIcon isSorted={sort.PastDowntimes}/></div>
             </DTHeader>
             <DTHeader colspan={3}>
               <div className="flex content-center cursor-pointer" onClick={()=>setSort({CurrentDowntimes: !sort.CurrentDowntimes})}><span>Current Downtimes </span><SortIcon isSorted={sort.CurrentDowntimes}/></div>
             </DTHeader>
-            <DTHeader colspan={3}>
+            <DTHeader className="bg-teal-100 dark:bg-gray-700" colspan={3}>
               <div className="flex content-center cursor-pointer" onClick={()=>setSort({FutureDowntimes: !sort.FutureDowntimes})}><span>Upcoming Downtimes </span><SortIcon isSorted={sort.FutureDowntimes}/></div>
             </DTHeader>
           </tr>
           <tr>
+            <DTHeader className="bg-teal-100 dark:bg-gray-700">Resource</DTHeader>
+            <DTHeader className="bg-teal-100 dark:bg-gray-700">Start</DTHeader>
+            <DTHeader className="bg-teal-100 dark:bg-gray-700">End</DTHeader>
+
             <DTHeader>Resource</DTHeader>
             <DTHeader>Start</DTHeader>
             <DTHeader>End</DTHeader>
-            
-            <DTHeader>Resource</DTHeader>
-            <DTHeader>Start</DTHeader>
-            <DTHeader>End</DTHeader>
-            
-            <DTHeader>Resource</DTHeader>
-            <DTHeader>Start</DTHeader>
-            <DTHeader>End</DTHeader>
+
+            <DTHeader className="bg-teal-100 dark:bg-gray-700">Resource</DTHeader>
+            <DTHeader className="bg-teal-100 dark:bg-gray-700">Start</DTHeader>
+            <DTHeader className="bg-teal-100 dark:bg-gray-700">End</DTHeader>
           </tr>
         </thead>
         <tbody>
           {sortedRows.map(dt=>
           <tr key={dt.SiteName} className="border border-gray-400">
-            <DTCell>{dt.SiteName}</DTCell>
-            <DTResourceList downtimes={dt.PastDowntimes}/>
-            <DTResourceList downtimes={dt.CurrentDowntimes}/>
-            <DTResourceList downtimes={dt.FutureDowntimes}/>
+            <DTCell className="bg-gray-100 dark:bg-gray-800">{dt.SiteName}</DTCell>
+            <DTResourceList className="bg-teal-50 dark:bg-gray-800" downtimes={dt.PastDowntimes}/>
+            <DTResourceList className="bg-blue-50 dark:bg-gray-800" downtimes={dt.CurrentDowntimes}/>
+            <DTResourceList className="bg-teal-50 dark:bg-gray-800" downtimes={dt.FutureDowntimes}/>
           </tr>)}
         </tbody>
       </table>
